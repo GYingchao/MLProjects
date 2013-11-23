4bd6dfb3daa4ed5003f2ca200ba776e94695cc33
Cui Yingchao <cseller@wf224-233.ust.hk>
2013-11-23 17:37:35 +0800

    There should be some mistake in logistic regression. When handling the same training data, the classification accuracy is extremely low.

--- project2/main.m
+++ project2/main.m
@@ -1,12 +1,12 @@
 % Project 2 test code snippets
 
 %% Test the naive implementation of logistic regression
-eta = 0.236;
+eta = 0.21;
 max = 100;
-trFile = 'liver_train.mat';
-teFile = 'liver_test.mat';
+trFile = 'mushroom_train.mat';
+teFile = 'mushroom_train.mat';
 
-for i=1:100
+for i=1:10
     max = max + 1*10;
     label = logisticRegression(trFile, teFile, eta, max);
 end
