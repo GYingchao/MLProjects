2d927239b69b5d60dba6e52eec4f200b12100187
Richard <cyc123bzd@gmail.com>
2013-11-28 11:56:35 +0800

    reg1

--- project2/logisticRegression.m
+++ project2/logisticRegression.m
@@ -1,9 +1,17 @@
 function [ result ] = logisticRegression( trData, teData, eta, maxIter )
 % logistic regression for binary classification using gradient descent algorithm
     % Load and format the input training set
+    startT = cputime;
     load(trData);
     [n d] = size(X);
     X1 = [ones(n, 1), X];
+    Y1 = Y;
+    clear X;
+    clear Y;
+    
+    load(teData);
+    n2 = size(Y, 1);
+    X2 = [ones(n2, 1), X];
     
     % Initialize important variables used in the algorithm
     iterations = 1;
@@ -15,7 +23,7 @@ function [ result ] = logisticRegression( trData, teData, eta, maxIter )
         for l=1:n
             o = X1(l, :)*W;
             a = 1.0/(1 + exp(-o));
-            a = 1 - Y(l, 1) - a;
+            a = 1 - Y1(l, 1) - a;
             for j=0:d
                dW(j+1, 1) = dW(j+1, 1) + a*X1(l, j+1);
             end
@@ -25,13 +33,8 @@ function [ result ] = logisticRegression( trData, teData, eta, maxIter )
     end
     % After convergence, we compute the discriminat function value directly
     % w.r.t testing data set
-    clear X;
-    clear Y;
     clear X1;
-   
-    load(teData);
-    n2 = size(Y, 1);
-    X2 = [ones(n2, 1), X];
+
     label = -1*ones(n2, 1);
     for l=1:n2
         label(l, 1) = X2(l, :)*W < 0;
@@ -42,5 +45,6 @@ function [ result ] = logisticRegression( trData, teData, eta, maxIter )
     %sprintf('%s%f%s%f', 'Accuracy achieved: ', ratio, ' where eta is ', eta)
     %result = label;
     result = ratio;
+    disp(cputime-startT);
 end
 
--- project2/main.m
+++ project2/main.m
@@ -3,21 +3,21 @@
 % Test the naive implementation of logistic regression
 
 % empirical study eta = 0.21
-startT = cputime;
-observation = zeros(1, 15);
+%startT = cputime;
+%observation = zeros(1, 15);
 eta = 0.04;
-max0 = 10;
+max = 1000;
 trFile = 'ionosphere_train.mat';
 teFile = 'ionosphere_test.mat';
-
+logisticRegression(trFile, teFile, eta, max);
 %for i=1:1
     %eta = eta0 + i/100;
-    for j=1:15
-        max = max0 + 100*j;
-        observation(j) = logisticRegression(trFile, trFile, eta, max);
-    end
+%     for j=1:15
+%         max = max0 + 100*j;
+%         observation(j) = logisticRegression(trFile, trFile, eta, max);
+%     end
 %end
-endT = cputime - startT;
+%endT = cputime - startT;
 % Test the naive implementation of KNN
 % for k=1:10
 %     kNN_classifier(k, trFile, teFile);
